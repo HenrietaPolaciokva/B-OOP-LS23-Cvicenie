fabf1679afcc37a2bfbae76d243faea96be51134
hena7 <xpolacikova@stuba.sk>
2023-02-22 15:24:44 +0100

    ok

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,19 @@
 package sk.stuba.fei.uim.oop;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static void Checknumber(int num) {
+        if (num > 10) {
+            System.out.println("1");
+        } else if (num == 10) {
+            System.out.println("0");
+        } else {
+            System.out.println("-1");
+        }
+    }
+
+    public static void main(String[] args) {
+        Checknumber(15);
     }
 }
+
