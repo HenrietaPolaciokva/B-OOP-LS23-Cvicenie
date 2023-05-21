44bf286d3f6c85f5c0f078c75a3cbfa33e123428
HenrietaPolaciokva <amjadaligolo477@gmail.com>
2023-05-21 23:40:23 +0500

    New Changes

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,7 +2,8 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
 
-    static void Checknumber(int num) {
+    static void Checknumber(int num)
+    {
         if (num > 10) {
             System.out.println("1");
         } else if (num == 10) {
@@ -12,8 +13,24 @@ public class Main {
         }
     }
 
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        // Check for divisibility from 2 to square root of the number
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;  // Number is divisible, hence not prime
+            }
+        }
+
+        return true;  // Number is prime
+    }
+
     public static void main(String[] args) {
         Checknumber(15);
+        System.out.println(isPrime(11));
     }
 }
 
